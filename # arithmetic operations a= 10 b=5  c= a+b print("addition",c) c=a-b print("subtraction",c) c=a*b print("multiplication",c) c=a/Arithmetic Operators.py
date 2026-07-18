db150c40b683fcd0b7c25a2e8f3aa88b5bb0391b
Rudra Karwande <karwanderr@gmail.com>
2026-07-18 15:04:52 +0530

    Add arithmetic operations with various operators

---
+++ # arithmetic operations a= 10 b=5  c= a+b print("addition",c) c=a-b print("subtraction",c) c=a*b print("multiplication",c) c=a/Arithmetic Operators.py
@@ -0,0 +1,14 @@
+# arithmetic operations
+a= 10
+b=5
+
+c= a+b
+print("addition",c)
+c=a-b
+print("subtraction",c)
+c=a*b
+print("multiplication",c)
+c=a/b
+print("division",c)
+c= a%b
+print("modulus",c)
