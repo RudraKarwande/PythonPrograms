4d36a1c58faf33e9f17d08c2ca4d985ba4687e52
Rudra Karwande <karwanderr@gmail.com>
2026-08-02 12:54:35 +0530

    Add tuple operations demonstration script
    This script demonstrates various tuple operations including creation, length, max/min values, indexing, slicing, concatenation, and repetition.

---
+++ Tuple_all_operations.py
@@ -0,0 +1,34 @@
+# 1.empty tupledeclarations
+tuple =()
+print(tuple)
+
+#2.tuple with number elemnts
+data = (2 , 4, 6, 8, 10 ,12 ,14, 16 , 18 , 20)
+print(data)
+
+#3. length of tuple 
+data = (2 , 4, 6, 8, 10 ,12 ,14, 16 , 18 , 20)
+print(len(data))
+
+#4. maximum and minimum in tuple
+data = (2 , 4, 6, 8, 10 ,12 ,14, 16 , 18 , 20)
+print (max(data))
+print (min(data))
+
+#5. indexing value in tuple
+data = (2 , 4, 6, 8, 10 ,12 ,14, 16 , 18 , 20)
+print (data[2])
+
+#6. slicing in tuple
+data = (2 , 4, 6, 8, 10 ,12 ,14, 16 , 18 , 20)
+print (data[2:6])
+
+#7. concatenation of tuple
+data = (2 , 4, 6, 8, 10 ,12 ,14, 16 , 18 , 20)
+new_data = data +(22,24,26,28,30)
+print(new_data)
+
+#8. repition of tuple
+data =(2 , 4, 6, 8, 10 ,12 ,14, 16 , 18 , 20)
+repeated_data= data *3
+print(repeated_data)
